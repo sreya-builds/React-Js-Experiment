515ebae71795b2da1262f0eb6b9b81061ae5a698
Shreya. <232308813+sreya-builds@users.noreply.github.com>
2025-12-17 18:22:17 +0530

    Enhance app.js with new exports and formatting
    Added additional exports for subjects and updated formatting.

--- Theory Class/Import export/app.js
+++ Theory Class/Import export/app.js
@@ -1,14 +1,17 @@
 
+//example1
 let obj = {
     user:'Shreya',
     age:20,
     city:'Noida',
     work:'Creative Web-dev'
 }
-
 export default obj;
 
-
+//example2
 export var add= (a,b) => a + b
 export var subtract = (a,b) => a-b;
 
+//example3
+export const maths = 80
+export const english = 90
